c508c2c9001db68b750eb92622688e7941b847bd
cliffordaustin <106864832+cliffordaustin@users.noreply.github.com>
2024-12-17 22:45:35 +0000

    updated github actions env

--- Backend/settings.py
+++ Backend/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["localhost", "job-project-backend.herokuapp.com"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "jobfinder-env.eba-twqe3t6p.us-west-2.elasticbeanstalk.com",
+]
 
 
 # Application definition
