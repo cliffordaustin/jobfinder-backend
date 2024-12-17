475ffeed5f5d64daf8fea1755ab4d63e621a330e
cliffordaustin <106864832+cliffordaustin@users.noreply.github.com>
2024-12-17 21:51:23 +0000

    updated action

--- Backend/settings.py
+++ Backend/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("JOB_PROJECT_SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = ["localhost", "job-project-backend.herokuapp.com"]
 
