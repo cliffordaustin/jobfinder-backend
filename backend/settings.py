e20562693173d826d5c34fe408f614b805fdb387
cliffordaustin <106864832+cliffordaustin@users.noreply.github.com>
2024-12-25 19:53:40 +0000

    updated cors settings

--- backend/settings.py
+++ backend/settings.py
@@ -121,7 +121,10 @@ MIDDLEWARE = [
     "allauth.account.middleware.AccountMiddleware",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://jobfinder-frontend-orcin.vercel.app",
+]
 
 ROOT_URLCONF = "backend.urls"
 
